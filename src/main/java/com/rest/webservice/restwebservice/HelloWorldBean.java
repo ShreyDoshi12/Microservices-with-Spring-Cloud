ee67052de814cfaa65a645580e3c29bcc478f924
Shrey Doshi <shrey.doshi94@gmail.com>
2020-02-12 00:04:28 -0600

    1st commit

---
+++ src/main/java/com/rest/webservice/restwebservice/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.rest.webservice.restwebservice;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String setMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
