75c5f9a84620420b59ab827e48a48f99b6efd884
Shrey Doshi <shrey.doshi@jbhunt.com>
2020-02-17 12:24:56 -0600

    Post, ResponseEntity and Exception Handling

--- src/main/java/com/rest/webservice/restwebservice/user/UserResource.java
+++ src/main/java/com/rest/webservice/restwebservice/user/UserResource.java
@@ -1,10 +1,11 @@
 package com.rest.webservice.restwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -13,18 +14,32 @@ public class UserResource {
     @Autowired
     private UserDAOService service;
 
-    //GET /users
-    //retrieve all UserResource
     @GetMapping(value = "/users")
     public List <User> retrieveAllUsers(){
+
         return service.findAll();
     }
 
-
-    //GET /users/{id}
-    //retrieveUser(int id)
     @GetMapping(value = "/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if(user == null)
+            throw new UserNotFoundException("id-" + id);
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+        //CREATED
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+      return ResponseEntity.created(location).build();
     }
 }
